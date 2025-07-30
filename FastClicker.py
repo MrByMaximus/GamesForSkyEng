f21c34b8b7b88a9f8b203a902da109815c585372
MrByMaximus <maksim.gildenberg@gmail.com>
2025-07-31 00:20:59 +0700

    Добавил подкраску при клике

--- FastClicker.py
+++ FastClicker.py
@@ -1,9 +1,13 @@
 import pygame as pg
 from random import randint
 
+pg.init()
+
 # Цвета
 LIGHT_BLUE = (127, 79, 194)
 YELLOW = (240, 221, 1)
+RED = (158, 0, 0)
+GREEN = (114, 223, 0)
 TEXT_COLOR = (0, 0, 0)
 
 # Размеры
@@ -39,9 +43,7 @@ class Area():
 #     def render_text(self, text):
 #         self.render(text, True, TEXT_COLOR, None)
 
-if __name__ == '__main__':
-    pg.init()
-
+def main():
     # Настройки счетчика кадров
     clock = pg.time.Clock()
     clock.tick(40)
@@ -51,7 +53,7 @@ if __name__ == '__main__':
     window.fill(color=LIGHT_BLUE)
 
     # Создание прямоугольника
-    rects = list()
+    rects: list[Area] = list()
     for i in range(1, RECT_COUNT+1):
         rects.append(Area(x=RECT_WIDTH*(i-1) + RECT_POS_X*i))
 
@@ -64,8 +66,11 @@ if __name__ == '__main__':
 
     # Игровой цикл
     isRunning = True
+    isClicked: bool | None = None
+
     last_ticks = pg.time.get_ticks()
     num = 1
+    card_clicked = 1
 
     while isRunning:
         for event in pg.event.get():
@@ -77,22 +82,39 @@ if __name__ == '__main__':
             elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 x, y = event.pos
                 for i in range(1, RECT_COUNT+1):
-                    if rects[i-1].collidepoint(x, y) and i == num:
-                        print("Попал")
+                    if rects[i-1].collidepoint(x, y):
+                        if i == num:
+                            isClicked = True
+                        else:
+                            isClicked = False
+                        card_clicked = i
                         break
 
         # Отрисовка прямоугольников
         for i in range(1, RECT_COUNT+1):
-            pg.draw.rect(window, YELLOW, rects[i-1].rect)
+            color = YELLOW
+            if i == card_clicked:
+                if isClicked == True:
+                    color = GREEN
+                if isClicked == False:
+                    color = RED
+            
+            pg.draw.rect(window, color, rects[i-1].rect)
 
-        # Отрисовка текста
+        # Генерация текста в прямоугольнике
         now_ticks = pg.time.get_ticks()
         if now_ticks - last_ticks >= DELAY:
             num = randint(1, RECT_COUNT)
             last_ticks = now_ticks
+            if isClicked != None:
+                isClicked = None
         
+        # Отрисовка текста
         window.blit(source=click_text, dest=(
             (RECT_WIDTH*(num-1) + RECT_POS_X*num) + 10,
             FONT_POS_Y))
 
         pg.display.update()
+
+if __name__ == '__main__':
+    main()
